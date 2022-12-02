e5386b960024a6603f073aee60505eac205d4903
Ashley Davies <ashley@davies.me.uk>
2022-12-02 07:09:22 +0000

    chore: day two

---
+++ AdventOfCode/DayTwo.fs
@@ -0,0 +1,62 @@
+module AdventOfCode.DayTwo
+
+open System.IO
+open FSharpx
+open Microsoft.FSharp.Collections
+open Helpers
+
+type Hand =
+    | Rock = 0
+    | Paper = 1
+    | Scissors = 2
+
+let predeterminedLossCode = Hand.Rock
+let predeterminedDrawCode = Hand.Paper
+let predeterminedVictoryCode = Hand.Scissors
+
+let whatWinsAgainst =
+    (flip Map.find) (Map [ (Hand.Rock, Hand.Paper); (Hand.Paper, Hand.Scissors); (Hand.Scissors, Hand.Rock) ])
+
+let baseScore = (flip Map.find) (Map [ (Hand.Rock, 1); (Hand.Paper, 2); (Hand.Scissors, 3) ])
+
+let outcomeScore a b =
+    if a = b then 3
+    elif a = whatWinsAgainst b then 6
+    else 0
+
+let scoreRound round =
+    match round with
+    | elf :: [ player ] -> baseScore player + outcomeScore player elf
+    | _ -> failwith "unknown round format"
+
+let scoreGame = List.map scoreRound >> List.sum
+
+let inputConversion =
+    Map [ ("A", Hand.Rock)
+          ("B", Hand.Paper)
+          ("C", Hand.Scissors)
+          ("X", Hand.Rock)
+          ("Y", Hand.Paper)
+          ("Z", Hand.Scissors) ]
+
+// For the second scenario, Rock -> must lose, Paper -> must draw, Scissors -> Must win
+let correctPlayerHand round =
+    match round with
+    | elf :: [ r ] when r = predeterminedLossCode -> elf :: [ elf |> whatWinsAgainst |> whatWinsAgainst ]
+    | elf :: [ r ] when r = predeterminedDrawCode -> elf :: [ elf ]
+    | elf :: [ r ] when r = predeterminedVictoryCode -> elf :: [ elf |> whatWinsAgainst ]
+    | _ -> failwith "unknown round format"
+
+let parseLine =
+    String.splitChar [| ' ' |]
+    >> List.ofSeq
+    >> List.map ((flip Map.find) inputConversion)
+
+let solution (args: string list) =
+    let lines =
+        File.ReadAllText args.Head
+        |> LineGrouping
+        |> List.map parseLine
+    
+    printfn $"Score: %i{ (lines |> scoreGame) }"
+    printfn $"Corrected score: %i{ (lines |> List.map correctPlayerHand |> scoreGame) }"
--- AdventOfCode/Program.fs
+++ AdventOfCode/Program.fs
@@ -2,17 +2,15 @@ module AdventOfCode.Program
 
 open Microsoft.FSharp.Collections
 open Microsoft.FSharp.Core
+open FSharpx
 
-let solutions = [
-    DayOne.solution
-]
+let solutions = [ DayOne.solution; DayTwo.solution ]
 
 [<EntryPoint>]
 let main argsArray =
-    let (dayNumber, args) =
+    let dayNumber, args =
         match (argsArray |> Array.toList) with
-        | dayNumString :: args -> (dayNumString |> int |> (-)1, args)
+        | dayNumString :: args -> (dayNumString |> int |> (flip (-) 1), args)
         | [] -> failwith "Please provide the day number as an argument"
-
     solutions[dayNumber] args
     0
